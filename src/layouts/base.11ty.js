0fb0a751acd9f2334c2a8d4301c5cf0d4105367d
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2021-07-06 00:09:37 +0300

    eleventy and rearrangement

---
+++ src/layouts/base.11ty.js
@@ -0,0 +1,59 @@
+module.exports.render = async function pwa({
+  lang = 'en',
+  title = '',
+  styles,
+  content,
+  scripts,
+  keywords = '',
+  description = '',
+}) {
+  return /* html */ `
+  <!DOCTYPE html>
+  <html lang="${lang}">
+    <head>
+      <meta charset="utf-8" />
+  
+      <title>${title}</title>
+  
+      <meta name="title" content="${title}" />
+      <meta name="description" content="${description}" />
+      <meta name="keywords" content="${keywords}" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=5"
+      />
+      <meta name="theme-color" content="#02021e" />
+  
+      <meta name="apple-mobile-web-app-status-bar-style" content="#02021e" />
+      <meta name="theme-color" content="#02021e" />
+      <meta property="og:title" content="${keywords}" />
+      <meta property="og:description" content="${description}" />
+      <meta property="og:type" content="website" />
+  
+      <!-- safari format detection -->
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="format-detection" content="address=no" />
+      <meta name="format-detection" content="email=no" />
+      <meta name="format-detection" content="date=no" />
+   
+      <!--  Here styles are inserted. -->
+			${(Array.isArray(styles) ? styles : Array.of(styles))
+        .filter(Boolean)
+        .map((url) => /* html */ `<link rel="stylesheet" href="${url}">`)
+        .reduce((text, link) => text + link, '')}
+    </head>
+    <body>
+      <div class="site-body">
+        ${content}
+      </div>
+      
+      <!--  Here scripts are inserted. -->
+			${(Array.isArray(scripts) ? scripts : Array.of(scripts))
+        .filter(Boolean)
+        .map((url) => /* html */ `<script defer src="${url}"></script>`)
+        .reduce((text, script) => text + script, '')}
+    </body>
+  </html>
+  `;
+};
